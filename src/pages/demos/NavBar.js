30b53372318d851b1b217645dea4e0b6d00bd5e7
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2020-02-16 07:53:16 +1100

    material-ui added

---
+++ src/pages/demos/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Modal from '../../components/modal/Modal'
+
+function Demos() {
+    return (
+        <div>
+            This is Demos
+
+        <div>
+            <Modal />
+        </div>
+        </div>
+    )
+
+
+}
+
+export default Demos;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Blog from '../blog/Blog'
+import Demos from '../demos/NavBar'
 
 function Main() {
     return (
         <div>
             This is Main Screen
-            <Blog />
+            <Demos />
         </div>
     )
 }
